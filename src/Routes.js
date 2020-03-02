dc2dd0b94eed33f9363f5d9eba698f731b8acd24
Hutch Crowley <tothecoda@gmail.com>
2020-03-01 23:39:59 -0600

    Update - app renders, API query need

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import App from './App'
 import { Route, Switch } from 'react-router-dom'
-import CarQuery from './components/CarQuery'
 
 const Routes = () => {
   return (
     <div>
       <Switch>
         <Route path='/'>
-          <CarQuery />
+          <App />
         </Route>
       </Switch>
     </div>
